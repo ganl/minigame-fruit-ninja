65c69724711f194628707f39760e34054f1c79dc
ganl <769323213@qq.com>
2018-07-14 15:20:20 +0800

    调整source目录结构

--- js/main.js
+++ js/main.js
@@ -1,178 +1,89 @@
-import Player from './player/index'
-import Enemy from './npc/enemy'
-import BackGround from './runtime/background'
-import GameInfo from './runtime/gameinfo'
-import Music from './runtime/music'
-import DataBus from './databus'
+var timeline = require( "timeline" );
+var tools = require( "tools" );
+var sence = require( "sence" );
+var Ucren = require( "lib/ucren" );
+var buzz = require( "lib/buzz" );
+var control = require( "control" );
+var csl = require( "object/console" );
+var message = require( "message" );
+var state = require( "state" );
 
-let ctx = canvas.getContext('2d')
-let databus = new DataBus()
+var game = require( "game" );
 
-/**
- * 游戏主函数
- */
-export default class Main {
-  constructor() {
-    // 维护当前requestAnimationFrame的id
-    this.aniId = 0
+var collide = require( "collide" );
 
-    this.restart()
-  }
+var setTimeout = timeline.setTimeout.bind( timeline );
 
-  restart() {
-    databus.reset()
+var log = function(){
+    var time = 1e3, add = 300, fn;
+    fn = function( text ){
+        setTimeout( function(){ csl.log( text ); }, time );
+        time += add;
+    };
+    fn.clear = function(){
+        setTimeout( csl.clear.bind( csl ), time );
+        time += add;
+    };
+    return fn;
+}();
 
-    canvas.removeEventListener(
-      'touchstart',
-      this.touchHandler
-    )
+exports.start = function(){
 
-    this.bg = new BackGround(ctx)
-    this.player = new Player(ctx)
-    this.gameinfo = new GameInfo()
-    this.music = new Music()
+    [ timeline, sence, control ].invoke( "init" );
 
-    this.bindLoop = this.loop.bind(this)
-    this.hasEventBind = false
+    log( "正在加载鼠标控制脚本" );
+    log( "正在加载图像资源" );
+	log( "正在加载游戏脚本" );
+    log( "正在加载剧情" );
+    log( "正在初始化" );
+	log( "正在启动游戏..." );
+    log.clear();
 
-    // 清除上一局的动画
-    window.cancelAnimationFrame(this.aniId);
+    setTimeout( sence.switchSence.saturate( sence, "home-menu" ), 3000 );
+};
 
-    this.aniId = window.requestAnimationFrame(
-      this.bindLoop,
-      canvas
-    )
-  }
+message.addEventListener("slice", function( knife ){
+    var fruits = collide.check( knife ), angle;
+    if( fruits.length )
+        angle = tools.getAngleByRadian( tools.pointToRadian( knife.slice(0, 2), knife.slice(2, 4) ) ),
+        fruits.forEach(function( fruit ){
+           message.postMessage( fruit, angle, "slice.at" );
+        });
+});
 
-  /**
-   * 随着帧数变化的敌机生成逻辑
-   * 帧数取模定义成生成的频率
-   */
-  enemyGenerate() {
-    if (databus.frame % 30 === 0) {
-      let enemy = databus.pool.getItemByClass('enemy', Enemy)
-      enemy.init(6)
-      databus.enemys.push(enemy)
+message.addEventListener("slice.at", function( fruit, angle ){
+
+    if( state( "sence-state" ).isnot( "ready" ) )
+        return ;
+
+    if( state( "sence-name" ).is( "game-body" ) ){
+        game.sliceAt( fruit, angle );
+        return ;
     }
-  }
 
-  // 全局碰撞检测
-  collisionDetection() {
-    let that = this
-
-    databus.bullets.forEach((bullet) => {
-      for (let i = 0, il = databus.enemys.length; i < il; i++) {
-        let enemy = databus.enemys[i]
-
-        if (!enemy.isPlaying && enemy.isCollideWith(bullet)) {
-          enemy.playAnimation()
-          that.music.playExplosion()
-
-          bullet.visible = false
-          databus.score += 1
-
-          break
-        }
-      }
-    })
-
-    for (let i = 0, il = databus.enemys.length; i < il; i++) {
-      let enemy = databus.enemys[i]
-
-      if (this.player.isCollideWith(enemy)) {
-        databus.gameOver = true
-
-        break
-      }
+    if( state( "sence-name" ).is( "home-menu" ) ){
+        fruit.broken( angle );
+        if( fruit.isHomeMenu )
+            switch( 1 ){
+                case fruit.isDojoIcon:
+                    sence.switchSence( "dojo-body" ); break;
+                case fruit.isNewGameIcon:
+                    sence.switchSence( "game-body" ); break;
+                case fruit.isQuitIcon:
+                    sence.switchSence( "quit-body" ); break;
+            }
+        return ;
     }
-  }
+});
 
-  // 游戏结束后的触摸事件处理逻辑
-  touchEventHandler(e) {
-    e.preventDefault()
+var tip = "";
 
-    let x = e.touches[0].clientX
-    let y = e.touches[0].clientY
+if( !Ucren.isChrome )
+    tip = "$为了获得最佳流畅度，推荐您使用 <span class='b'>Google Chrome</span> 体验本游戏";
 
-    let area = this.gameinfo.btnArea
+if( !buzz.isSupported() )
+    tip = tip.replace( "$", "您的浏览器不支持 &lt;audio&gt 播放声效，且" );
 
-    if (x >= area.startX
-      && x <= area.endX
-      && y >= area.startY
-      && y <= area.endY)
-      this.restart()
-  }
+tip = tip.replace( "$", "" );
 
-  /**
-   * canvas重绘函数
-   * 每一帧重新绘制所有的需要展示的元素
-   */
-  render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    this.bg.render(ctx)
-
-    databus.bullets
-      .concat(databus.enemys)
-      .forEach((item) => {
-        item.drawToCanvas(ctx)
-      })
-
-    this.player.drawToCanvas(ctx)
-
-    databus.animations.forEach((ani) => {
-      if (ani.isPlaying) {
-        ani.aniRender(ctx)
-      }
-    })
-
-    this.gameinfo.renderGameScore(ctx, databus.score)
-
-    // 游戏结束停止帧循环
-    if (databus.gameOver) {
-      this.gameinfo.renderGameOver(ctx, databus.score)
-
-      if (!this.hasEventBind) {
-        this.hasEventBind = true
-        this.touchHandler = this.touchEventHandler.bind(this)
-        canvas.addEventListener('touchstart', this.touchHandler)
-      }
-    }
-  }
-
-  // 游戏逻辑更新主函数
-  update() {
-    if (databus.gameOver)
-      return;
-
-    this.bg.update()
-
-    databus.bullets
-      .concat(databus.enemys)
-      .forEach((item) => {
-        item.update()
-      })
-
-    this.enemyGenerate()
-
-    this.collisionDetection()
-
-    if (databus.frame % 20 === 0) {
-      this.player.shoot()
-      this.music.playShoot()
-    }
-  }
-
-  // 实现游戏帧循环
-  loop() {
-    databus.frame++
-
-    this.update()
-    this.render()
-
-    this.aniId = window.requestAnimationFrame(
-      this.bindLoop,
-      canvas
-    )
-  }
-}
+Ucren.Element( "browser" ).html( tip );
